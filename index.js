94f0e209264e8e95c82b7156ff7f0b48f31a51b1
Monchol <64288365+Monchol@users.noreply.github.com>
2023-02-16 22:10:32 -0300

    Init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const navButton = document.querySelector(".nav_button");
+const links = document.querySelector(".show_nav_mobile_about");
+const navMobile = document.querySelector(".nav_mobile");
+navMobile.style.boxShadow = "-2px 0 12px #000";
+
+navButton.addEventListener("click", function(){
+    if(links.classList.contains("nav_mobile_about")){
+        links.classList.remove("nav_mobile_about");
+        navMobile.style.boxShadow = ""
+        navMobile.style.transition = "250ms"
+    } else {
+        links.classList.add("nav_mobile_about");
+        navMobile.style.boxShadow = "-2px 0 12px #000";
+        navMobile.style.transition = "250ms"
+    }
+});
+
